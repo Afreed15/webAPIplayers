795fa95fe904022ac49efa4ae0c69aece7277ab7
Afreed15 <Afreed.vs786@gmail.com>
2021-03-17 12:54:33 +0530

    my commit

---
+++ Controllers/PlayersController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApimodelbase.Models;
+
+namespace WebApimodelbase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        static List<Players> listplayers = new List<Players>
+        {
+            new Players{PId=1,PName="rohit",PDob=DateTime.Parse("12/2/1992"),PTeam="MI"},
+            new Players{PId=2,PName="Polard",PDob=DateTime.Parse("12/2/1989"),PTeam="MI"},
+            new Players{PId=3,PName="Hardik",PDob=DateTime.Parse("12/11/1993"),PTeam="MI"}
+        };
+        [HttpGet]
+        public IEnumerable<Players> Get()
+        {
+            return listplayers.ToList();
+        }
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var player = listplayers.FirstOrDefault(x => x.PId == id);
+            if(player==null)
+            {
+                return NotFound();
+            }
+            return Ok(player);
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody] Players obj)
+        {
+            Players player = new Players();
+            player.PId = obj.PId;
+            player.PName = obj.PName;
+            player.PTeam = obj.PTeam;
+            player.PDob = obj.PDob;
+            listplayers.Add(obj);
+            return NoContent();
+        }
+        [HttpPut("{id}")]
+        public ActionResult Put(int id,[FromBody] Players obj)
+        {
+            var player = listplayers.FirstOrDefault(x => x.PId == id);
+            if(player==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+               
+                player.PName = obj.PName;
+                player.PTeam = obj.PTeam;
+                player.PDob = obj.PDob;
+                listplayers.Add(player);
+
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var player = listplayers.FirstOrDefault(x => x.PId == id);
+            if(player==null)
+            {
+                return NotFound();
+            }else
+            {
+                listplayers.RemoveAt(id);
+            }
+            return NoContent();
+
+
+        }
+        
+    }
+}
---
+++ Models/Players.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApimodelbase.Models
+{
+    public class Players
+    {
+        public int PId   { get; set; }
+        public string PName { get; set; }
+        public DateTime PDob { get; set; }
+        public string PTeam { get; set; }
+    }
+}
